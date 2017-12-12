49dde5d50ba754faed2242e9c85764098ab80b5b
Ruth Kitchin Tillman <ruthtillman@gmail.com>
2017-12-12 16:20:19 -0500

    Started work on requests and handling sesssions
    Got through reading configs and developing sessions.

--- update-URIs.py
+++ update-URIs.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 # Why does this script exist? This script exists so that we can work separately with ASpace data, create a spreadsheet (CSV) of the IDs of subjects which should be updated to include any authority_id but really probably a URI, and then rewrite downloads of those subjects as JSON files. The data to create this sheet and the JSON downloading info should be done separately. The data will probably come from MySQL queries. The downloads will probably come from a bash file. The bash file may then be called upon to run this Python script after using the same CSV to download the appropriate JSON files.
 
-import os, requests, json, logging, csv
+import os, requests, json, logging, csv, ConfigParser
 
 # Check to be sure that the URI is empty
 # Tasks:
 # Handle session ID and ASpace API URI as params to pass to the script variables
-# Open CSV and call function to handle its two variables
-# Create function to work with the two variables (do this first and then the CSV?)
 # Write scripts to download via cURL and then post back. (ok so figure out how cURL works in Python or shift over to bash)
+
+# Sets up config stuff for validation. Borrowed from ArchivesSnake-linked Duke scripts
+
+configFilePath = 'local_settings.cfg'
+config = configparser.ConfigParser()
+config.read(configFilePath)
+
+# From Duke: URL parameters dictionary, used to manage common URL patterns
+
+dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'),'password': config.get('ArchivesSpace', 'password')}
+baseURL = dictionary['baseURL']
+repositoryBaseURL = '{baseURL}/repositories/{repository}/'.format(**dictionary)
+
+# authenticates the session
+auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session}
 
 def update_log(logStatus, uri,jsonFile,logFile):
     # It may eventually be useful to write a script which takes the beginning of the ALREADY EXISTS and does a check on those files to get the actual values to ensure it's not blank or otherwise a bad authority.
@@ -41,3 +56,9 @@ csvName = input("Enter the CSV name: ")
 logFile = input("Enter the log file name: ")
 
 read_CSV(csvName,logFile)
+
+
+
+ASpaceHeader = "X-ArchivesSpace-Session:" + $SESSION
+
+SESSION = input("Enter the ASpace session key: ")
