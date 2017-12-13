04013ed8cff228523bfa91ff1e8bebf4357d2ac2
Ruth Kitchin Tillman <ruthtillman@gmail.com>
2017-12-13 16:15:48 -0500

    Cleanup and comments of update-URIs
    Added descriptive comments for the major functions of update-URIs

--- update-URIs.py
+++ update-URIs.py
@@ -4,8 +4,7 @@
 # A second script will use the same CSV to post those subject records back to the server. A second script is desireable so that spot-checking of the records may be completed before it's run. This could be done by combining and pausing this script, but time should be allowed.
 # The data will probably come from MySQL queries.
 
-import os, requests, json, logging, csv, configparser
-import datetime
+import os, requests, json, logging, csv, configparser, datetime
 
 # Check to be sure that the URI is empty
 # Tasks:
@@ -20,9 +19,8 @@ config.read(configFilePath)
 
 # From Duke: URL parameters dictionary, used to manage common URL patterns
 
-dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'repository':config.get('ArchivesSpace', 'repository'), 'user': config.get('ArchivesSpace', 'user'),'password': config.get('ArchivesSpace', 'password')}
+dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'user': config.get('ArchivesSpace', 'user'),'password': config.get('ArchivesSpace', 'password')}
 baseURL = dictionary['baseURL']
-repositoryBaseURL = '{baseURL}/repositories/{repository}/'.format(**dictionary)
 subjectBaseURL ='{baseURL}/subjects/'.format(**dictionary)
 
 # authenticates the session
@@ -30,7 +28,8 @@ auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session}
 
-# Adapt this to open the CSV and do the call based on subject base URLs [note, this involved editing to change from repository base URL although that should be stored for handling the updates for DIGITAL OBJECTS]
+# Opens the CSV which has been input by the user. Opens it as a CSV reader, starts a download log file and opens that to write the confirmations of what's been downloaded (this is not _true_ logging). For each row the CSV, send an API get request with already-generated headers based on the base API URL for subjects and the subject ID as taken from the id column of the CSV. Interprets response as JSON. Uses the same id to create a filename and dumps the JSON response into the file. Then writes a log entry presuming that the download was successful at the approx timestamp (note, it will be off by at most a second)... the assumption is that any breaks in the process will stop the log from being written but the errors won't be written to the log yet.
+
 def download_subjects(csvName):
     with open(csvName, newline='') as data:
         reader = csv.DictReader(data)
@@ -44,14 +43,17 @@ def download_subjects(csvName):
                 log = datetime.datetime.now().isoformat() + "\t" + subjectFile + " downloaded\n"
                 logging.write(log)
 
+# Takes a set of inputs related to an action just performed and either writes a positive or negative status with those log values into the log. The tab-separation means this can be used in spreadsheet format (as TSV).
+
 def update_log(logStatus, uri,jsonFile,logFile):
-    # It may eventually be useful to write a script which takes the beginning of the ALREADY EXISTS and does a check on those files to get the actual values to ensure it's not blank or otherwise a bad authority.
     if logStatus == 1:
         log = datetime.datetime.now().isoformat() + "\tSUCCESS\t" + jsonFile + "\tupdated to include " + uri + " and file new-" + jsonFile + " created.\n"
     if logStatus == 0:
         log = datetime.datetime.now().isoformat() + "\tALREADY EXISTS\t" + jsonFile + "\talready contains a value for authority_id and " + uri + " was not added.\n"
     with open(logFile, "a") as logging:
         logging.write(log)
+
+# opens the JSON file for each subject record, loads it as a JSON object, tests to see if the file contains an authority_id field, if not it adds a new field with the value of the uri it received from process_CSV as its value. It then writes a new JSON file which uses the same identifier, but adds a pre-fix new. The function calls the logging function and passes on a status dependent on whether it found an authority_id and created a new file or not.
 
 def write_URI(subject_id,uri,logFile):
     jsonFile = subject_id + '.json'
@@ -64,6 +66,8 @@ def write_URI(subject_id,uri,logFile):
         update_log(1,uri,jsonFile,logFile)
     else:
         update_log(0,uri,jsonFile,logFile)
+
+# Opens the CSV which has been provided as input by the user, opens and passes to a CSV parser, and for each row calls the function to add test for and add authority_ids if they don't exist, passing along the values for the id column and URI column from the CSV along with the name of the log file for homebrewed logging purposes.
 
 def process_CSV(csvName,logFile):
     with open(csvName, newline='') as data:
