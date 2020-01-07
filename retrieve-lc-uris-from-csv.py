bc28562cb58337f71f443317c06be14f647d58f6
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-07 14:02:38 -0500

    code opens and reads CSV, then URL escapes each row. Next up is actually getting the URIs, then writing it out to file.

--- retrieve-lc-uris-from-csv.py
+++ retrieve-lc-uris-from-csv.py
@@ -1,4 +1,4 @@
-import requests, csv
+import requests, csv, os
 
 # Steps to take:
 # open CSV and read title
@@ -10,3 +10,22 @@ import requests, csv
 #  subject = thing.headers['X-Uri']
 #  confirmLabel = thing.headers['X-Preflabel']
 #  write these into the CSV
+
+def URI_escape(value):
+  return value.replace(' -- ', '--').replace(' ', '%20').replace(',', '%C').replace("'","%27").replace('(', '%28').replace(')', '%29')
+
+def get_subject_URIs(writer,csvSource):
+  with open(csvSource, newline='') as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+      subjectLabel = URI_escape(row['subject'])
+
+def write_subject_csv(csvOutput,csvSource):
+    fieldnames = ['ASpaceID', 'subject', 'LC_URI', 'LC_Label']
+    with open(csvName, 'w', newline='') as outputFile:
+        writer = csv.DictWriter(outputFile, fieldnames=fieldnames)
+        writer.writeheader()
+        get_subject_URIs(writer,csvSource)
+
+csvOutput='recon-test/scl_recon_2020_01_07.csv'
+csvSource='recon-test/scl_lcsh_2020_01_06.csv'
