cc6776fefdcf4476888a6cb8cc2df111043809f3
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-08 12:56:47 -0500

    made upload subjects into an ASnake script

--- upload-subjects.py
+++ upload-subjects.py
@@ -1,48 +1,21 @@
 #!/usr/bin/env python
 
 # Creating a script which will take all "new-" subjects created by update-URIs.py and upload them to ArchivesSpace.
-# subjects['title'] - creating new subjects.
-# posts['id']
 
-import os, requests, glob, json, logging, csv, configparser, datetime
+import os, json, glob
 
-configFilePath = 'local_settings.cfg'
-config = configparser.ConfigParser()
-config.read(configFilePath)
+from asnake.client import ASnakeClient
+client = ASnakeClient()
+client.authorize()
 
-# From Duke: URL parameters dictionary, used to manage common URL patterns
-
-dictionary = {'baseURL': config.get('ArchivesSpace', 'baseURL'), 'user': config.get('ArchivesSpace', 'user'),'password': config.get('ArchivesSpace', 'password')}
-baseURL = dictionary['baseURL']
-subjectBaseURL ='{baseURL}subjects/'.format(**dictionary)
-
-# authenticates the session
-auth = requests.post('{baseURL}/users/{user}/login?password={password}&expiring=false'.format(**dictionary)).json()
-session = auth["session"]
-headers = {'X-ArchivesSpace-Session':session}
-
-def update_log(resultsGroup,logName,jobStart,jobEnd):
-    # The log is created incrimentally because json.loads runs into various issues with these variables, so it's more straightforward to just add
-    log = json.loads('{"jobType" : "upload_subjects"}')
-    log['user'] = dictionary['user']
-    log['dateTimeStart'] = jobStart
-    log['dateTimeEnd'] = jobEnd
-    log['responses'] = resultsGroup
-    with open(logName, "a") as logging:
-        json.dump(log, logging, indent=4)
-
-def upload_new_subjects():
+def upload_new_subjects(JSONSource,client):
+    os.chdir(JSONSource)
     files = glob.glob("new-*.json")
-    logName = datetime.datetime.now().strftime('%Y-%m-%d-T-%H-%M') + "_subject_upload_log.json"
-    jobStart = datetime.datetime.now().isoformat()
-    results = []
     for jsonFile in files:
         subject = json.load(open(jsonFile))
         subject_id = jsonFile[4:-5]
-        posts = requests.post(subjectBaseURL + subject_id, data=json.dumps(subject), headers=headers).json()
-        posts['update_time'] = datetime.datetime.now().isoformat()
-        results.append(posts)
-    jobEnd = datetime.datetime.now().isoformat()
-    update_log(results,logName,jobStart,jobEnd)
+        response = client.post("/subjects/" + subject_id, data=json.dumps(subject)).json()
+        response
 
-upload_new_subjects()
+JSONSource=input("Enter the source directory of new JSON Files: ")
+upload_new_subjects(JSONSource,client)
