a265e19056ca6c1d1e2612a4ecf7704fbb3c621e
Ruth Kitchin Tillman <rkt@ruthtillman.com>
2020-01-09 08:57:28 -0500

    adding documentation on how to solve escaping issue with furls

--- retrieve-lc-uris-from-csv.py
+++ retrieve-lc-uris-from-csv.py
@@ -1,10 +1,20 @@
-import requests, csv, os, time
+import requests, csv, os, time, furls
 
 # Steps to take:
 # if thing.response_code == 200:
 #  subject = thing.headers['X-Uri']
 #  confirmLabel = thing.headers['X-Preflabel']
 #  write these into the CSV
+
+# for url, build instead using furls and escape using furls. Note we'll need to combine the initial `authorities/subjects/label/` and the subject when escaping.
+#>>> subject = 'World War, 1939-1945--Personal narratives, American'
+#>>> subpath = 'authorities/subjects/label/'
+#>>> f = furl('http://id.loc.gov/')
+#>>> f.path = subpath + subject
+#>>> f.url
+#'http://id.loc.gov/authorities/subjects/label/World%20War,%201939-1945--Personal%20narratives,%20American'
+#>>> requests.get(f.url).headers['X-PrefLabel']
+#'World War, 1939-1945--Personal narratives, American'
 
 def URI_escape(value):
   return value.replace(' -- ', '--').replace(' ', '%20').replace(',', '%2C').replace("'","%27").replace('(', '%28').replace(')', '%29')
